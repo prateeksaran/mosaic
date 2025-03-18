8170c3409730e85d77a732eb2248997558e062c8
prateeksaran <prateeksaransri@gmail.com>
2025-03-18 20:33:03 +0530

    first commit

---
+++ sript.js
@@ -0,0 +1,16 @@
+function toggleDropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+// Close dropdown when clicking outside
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
